fc308be5fd0bdc469676a8fe8f0bd2fba56f91f0
jobum97 <brian654321@naver.com>
2024-02-12 16:59:28 +0900

    feature/feature/kis-api: kis api token webclient에 심어주기

---
+++ src/main/kotlin/com/dormammu/tradingsimulation/config/dto/KisApiEnv.kt
@@ -0,0 +1,9 @@
+package com.dormammu.tradingsimulation.config.dto
+
+data class KisApiEnv(
+    private var kisApiKey: String,
+    private var kisSecretApiKey: String,
+    private var kisToken: String
+){
+
+}
